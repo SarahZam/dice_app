4f7b9507048aec59ea97c300e1054247b5d1b4e0
SarahZam <szaman@u.rochester.edu>
2021-02-15 20:33:26 -0500

    Flat button

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-  ,
-    )
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Dicee'),
+          ),
+          backgroundColor: Colors.red[800],
+        ),
+        body: DicePage(),
+      ),
+    ),
   );
 }
 
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            //takes twice as wide
+            //the flex property is just a ratio
+            //flex: 2,
+            child: FlatButton(
+              onPressed: ,
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice2.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
